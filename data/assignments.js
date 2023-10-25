073a97488e74b3f66642d6025945d1ff3162bbc6
planit <planit@tanjeffreyz.github.io>
2023-10-25 23:37:07 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -104,6 +104,13 @@ assignments = {
       "dueDate": "2023-11-01T22:00:00",
       "link": "https://www.gradescope.com/courses/580340",
       "submitted": false
+    },
+    {
+      "title": "Homework 9 Coding Portion",
+      "course": "CS 170",
+      "dueDate": "2023-10-31T22:00:00",
+      "link": "https://www.gradescope.com/courses/580340",
+      "submitted": false
     }
   ],
   "CS 61C": [
