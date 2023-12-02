f9fe60dbd3f96697c6604428b35a204ebc0d0735
planit <planit@tanjeffreyz.github.io>
2023-12-02 05:37:22 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -244,8 +244,8 @@ assignments = {
       "title": "Project 3B",
       "course": "CS 61C",
       "dueDate": "2023-11-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/579646",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/579646/assignments/3481933/submissions/217021845",
+      "submitted": true
     },
     {
       "title": "Project 4",
