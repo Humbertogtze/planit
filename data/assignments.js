bb50416764df981748bf748aa3647723fb630890
planit <planit@tanjeffreyz.github.io>
2024-01-18 06:39:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,4 +1,12 @@
 assignments = {
-  "EECS 126-LEC-001": [],
+  "EECS 126-LEC-001": [
+    {
+      "title": "Homework 1",
+      "course": "EECS 126-LEC-001",
+      "dueDate": "2024-01-23T23:00:00",
+      "link": "https://www.gradescope.com/courses/696086",
+      "submitted": false
+    }
+  ],
   "Math 110.114 Quizzes": []
 };
