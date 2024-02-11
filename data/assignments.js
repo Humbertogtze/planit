54b711a64ffed7c7f36c7c96806ce93e04d1ca01
planit <planit@tanjeffreyz.github.io>
2024-02-11 02:37:19 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,13 @@ assignments = {
       "dueDate": "2024-02-08T20:59:00",
       "link": "https://www.gradescope.com/courses/689970/assignments/4025587/submissions/229059989",
       "submitted": true
+    },
+    {
+      "title": "Homework 3",
+      "course": "CS171",
+      "dueDate": "2024-02-13T20:59:00",
+      "link": "https://www.gradescope.com/courses/689970",
+      "submitted": false
     }
   ],
   "EECS 126-LEC-001": [
