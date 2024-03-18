fab2e391832895f027d74f73352c6ca4e61b3092
planit <planit@tanjeffreyz.github.io>
2024-03-18 23:37:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -202,8 +202,8 @@ assignments = {
       "title": "Mid-semester Feedback Survey (https://forms.gle/HAgSLxpy5htGQbSn7)",
       "course": "EECS 126-LEC-001",
       "dueDate": "2024-03-24T23:59:00",
-      "link": "https://www.gradescope.com/courses/696086",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/696086/assignments/4226596/submissions/239957164",
+      "submitted": true
     },
     {
       "title": "Homework 8",
