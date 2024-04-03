8ef32ac91dae95e34b48e1a5f8068e03b1e1d5e7
planit <planit@tanjeffreyz.github.io>
2024-04-03 20:37:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -236,7 +236,7 @@ assignments = {
     {
       "title": "Lab 3 Self-grade",
       "course": "EECS 126-LEC-001",
-      "dueDate": "2024-04-05T23:00:00",
+      "dueDate": "2024-04-12T23:00:00",
       "link": "https://www.gradescope.com/courses/696086/assignments/4237947/submissions/new",
       "submitted": false
     }
