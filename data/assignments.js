8c189eaf5832dbc66bc4f82270d3352fa5860171
planit <planit@tanjeffreyz.github.io>
2024-04-11 05:37:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -260,6 +260,13 @@ assignments = {
       "dueDate": "2024-04-16T23:59:00",
       "link": "https://www.gradescope.com/courses/696086",
       "submitted": false
+    },
+    {
+      "title": "Lab 4",
+      "course": "EECS 126-LEC-001",
+      "dueDate": "2024-04-19T23:59:00",
+      "link": "https://www.gradescope.com/courses/696086",
+      "submitted": false
     }
   ],
   "MATH 113": [
