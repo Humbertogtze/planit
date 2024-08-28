905f423f8ce1cc0bbfda6b3946a0cb91bc9d2ddc
planit <planit@tanjeffreyz.github.io>
2024-08-28 07:38:15 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,5 +1,13 @@
 assignments = {
   "CS 162": [],
-  "CS 189/289A": [],
+  "CS 189/289A": [
+    {
+      "title": "HW 1 Written",
+      "course": "CS 189/289A",
+      "dueDate": "2024-09-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/814736",
+      "submitted": false
+    }
+  ],
   "MATH 185-LEC-002": []
 };
