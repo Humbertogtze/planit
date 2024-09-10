c602dca7f98b47d22c2933e4a33ede4e2ec25d6d
planit <planit@tanjeffreyz.github.io>
2024-09-10 01:54:33 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -52,6 +52,13 @@ assignments = {
       "dueDate": "2024-09-16T23:30:00",
       "link": "https://www.gradescope.com/courses/847154/assignments/4895210/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Lecture 4 warmup",
+      "course": "CS 270",
+      "dueDate": "2024-09-18T16:00:00",
+      "link": "https://www.gradescope.com/courses/847154/assignments/4927855/submissions/new",
+      "submitted": false
     }
   ],
   "MATH 185-LEC-002": [
