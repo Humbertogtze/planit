8a16562629ccae2ac744d97bf3ffb49a8b7ccbad
planit <planit@tanjeffreyz.github.io>
2024-09-24 17:40:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -124,6 +124,13 @@ assignments = {
       "dueDate": "2024-09-25T23:59:00",
       "link": "https://www.gradescope.com/courses/840113",
       "submitted": false
+    },
+    {
+      "title": "Homework 5",
+      "course": "MATH 185-LEC-002",
+      "dueDate": "2024-10-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/840113",
+      "submitted": false
     }
   ]
 };
