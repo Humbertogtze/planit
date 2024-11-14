886b16c3bc5953f8fa2ff7ba313dc06ad34083e2
planit <planit@tanjeffreyz.github.io>
2024-11-14 18:45:51 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -108,6 +108,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "HW 6 Code",
+      "course": "CS 189/289A",
+      "dueDate": "2024-11-22T23:59:00",
+      "link": "https://www.gradescope.com/courses/814736",
+      "submitted": false
+    },
+    {
       "title": "HW 3 Code",
       "course": "CS 189/289A",
       "dueDate": "2024-10-09T23:59:00",
