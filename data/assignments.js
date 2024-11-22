0b1979fcb34ca71aa733d69d7bf922accfd8ec00
planit <planit@tanjeffreyz.github.io>
2024-11-22 19:37:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -69,6 +69,13 @@ assignments = {
       "dueDate": "2024-11-18T22:00:00",
       "link": "https://www.gradescope.com/courses/859783",
       "submitted": false
+    },
+    {
+      "title": "Homework 10",
+      "course": "CS 172",
+      "dueDate": "2024-11-25T22:00:00",
+      "link": "https://www.gradescope.com/courses/859783",
+      "submitted": false
     }
   ],
   "CS 189/289A": [
