710dd69c7a9f3a680b50ca2301d318ad132bc575
planit <planit@tanjeffreyz.github.io>
2025-02-01 08:40:04 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -4,8 +4,8 @@ assignments = {
       "title": "Homework 1",
       "course": "CS 174",
       "dueDate": "2025-01-31T17:00:00",
-      "link": "https://www.gradescope.com/courses/954359",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/954359/assignments/5665744/submissions/304150083",
+      "submitted": true
     }
   ]
 };
