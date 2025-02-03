a8d06c6f81d25258b2b29b6ea96461726bb90946
planit <planit@tanjeffreyz.github.io>
2025-02-03 07:37:17 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -6,6 +6,13 @@ assignments = {
       "dueDate": "2025-01-31T17:00:00",
       "link": "https://www.gradescope.com/courses/954359/assignments/5665744/submissions/304150083",
       "submitted": true
+    },
+    {
+      "title": "Homework 2",
+      "course": "CS 174",
+      "dueDate": "2025-02-07T17:00:00",
+      "link": "https://www.gradescope.com/courses/954359",
+      "submitted": false
     }
   ],
   "EECS 182/282A": [
